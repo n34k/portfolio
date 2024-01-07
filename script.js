91f7cdd6d0f5d335879c6484b28cd59a352809bd
Nick Davis <ndavis55862@gmail.com>
2024-01-06 20:30:58 -0800

    Add scroll fade in

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const DOM = (() => {
     const name = document.querySelector('.biggest-text');
     const welcome = document.querySelector('.bigger-text');
     const learn = document.querySelector('.bottom-text');
+    const aboutMe = document.querySelector('.about-me');
+    const work = document.querySelector('.work');
 
     const fadeEvent = () => {
         hello.classList.add("fade");
@@ -17,8 +19,29 @@ const DOM = (() => {
         }, 3000);
     }
 
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+      }
+
+    const fadeScroll = () => {
+        if (isInViewport(aboutMe)) {
+            aboutMe.classList.add('fade');
+          }
+    
+          if (isInViewport(work)) {
+            work.classList.add('fade');
+          }
+    }
+
     const fadeLoad = () => {
         document.addEventListener("DOMContentLoaded" , fadeEvent);
+        window.addEventListener('scroll', fadeScroll);
     }
     return{fadeLoad}
 })();
